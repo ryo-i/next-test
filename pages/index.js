18ace7427a04a9592b9142f48ce7372e4909cb3f
ryo-i <iidazz0109@gmail.com>
2021-04-22 06:56:11 +0900

    ヘッダー、フッター追加

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Link from 'next/link';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function HomePage() {
     return <div>
-            <p>ねくすと・じぇいえす事始め!</p>
-            <Link href="/other">
-                <a>別ページへ</a>
-            </Link>
-      </div>;
+            <Header />
+            <section>
+                <h2>トップページ</h2>
+                <p>Next.jsの世界にようこそ！</p>
+                <Link href="/other"><a>別ページへ</a></Link>
+            </section>
+            <Footer />
+        </div>;
   }
 
   export default HomePage;
--- pages/other.js
+++ pages/other.js
@@ -1,11 +1,16 @@
 import Link from 'next/link';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Other() {
     return <div>
-            <p>ここは別のページだよ〜ん!</p>
-            <Link href="/">
-                <a>戻る</a>
-            </Link>
+            <Header />
+            <section>
+                <h2>別ページ</h2>
+                <p>ここは別のページだよ〜ん!</p>
+                <Link href="/"><a>戻る</a></Link>
+            </section>
+            <Footer />
         </div>;
   }
 
