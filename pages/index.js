d85efcc3d6ed594e984716ebf116fd3eea35ec3c
ryo-i <iidazz0109@gmail.com>
2021-04-22 07:21:43 +0900

    headタグ設定

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,29 @@
 import Link from 'next/link';
+import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const text = {
+    title: 'トップページ',
+    desctiption: 'Next.jsの世界にようこそ！'
+};
+
 function HomePage() {
-    return <div>
+    return (
+        <div>
+            <Head>
+                <title>{ text.title }</title>
+                <meta name="description" content={ text.desctiption } />
+            </Head>
             <Header />
             <section>
-                <h2>トップページ</h2>
-                <p>Next.jsの世界にようこそ！</p>
+                <h2>{ text.title }</h2>
+                <p>{ text.desctiption }</p>
                 <Link href="/other"><a>別ページへ</a></Link>
             </section>
             <Footer />
-        </div>;
+        </div>
+    );
   }
 
   export default HomePage;
--- pages/other.js
+++ pages/other.js
@@ -1,17 +1,29 @@
 import Link from 'next/link';
+import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const text = {
+    title: '別ページ',
+    desctiption: 'ここは別のページだよ〜ん!'
+};
+
 function Other() {
-    return <div>
+    return (
+        <div>
+             <Head>
+                <title>{ text.title }</title>
+                <meta name="description" content={ text.desctiption } />
+            </Head>
             <Header />
             <section>
-                <h2>別ページ</h2>
-                <p>ここは別のページだよ〜ん!</p>
+                <h2>{ text.title }</h2>
+                <p>{ text.desctiption }</p>
                 <Link href="/"><a>戻る</a></Link>
             </section>
             <Footer />
-        </div>;
+        </div>
+    );
   }
 
   export default Other;
